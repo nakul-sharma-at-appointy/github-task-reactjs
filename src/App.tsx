ba023cf805f12f8dbd47c1bdfd8b427e43765058
Nakul Sharma <nakul@appointy.com>
2023-09-15 10:44:28 +0530

    refactor: removed unnecessary styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
 import Header from './components/Header';
 import { Container } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  root: {
-    margin: "0px",
-    padding: "0px",
-    boxSizing: "border-box",
-  },
-});
-
 function App() {
-  const classes = useStyles();
 
   return (
-    <Container maxWidth={false} disableGutters={true} className={`App ${classes.root}`}>
+    <Container maxWidth={false} disableGutters={true}>
       <Header />
     </Container>
   );
